c9a2238511613798104e9acbd5b0aebf6b60741e
Ariel Lighty <ariellig@buffalo.edu>
2025-10-04 13:35:45 -0400

    fix jwk public storage

--- netlify/functions/jwks.ts
+++ netlify/functions/jwks.ts
@@ -1,17 +1,13 @@
 import { Handler } from '@netlify/functions';
 
 const PUBLIC_JWK = {
-  keys: [
-    {
-      "kty": "EC",
-      "x": "3sVbr4xwN7UtmG1L19vL0x9iN-FRcl7p-Wja_xPbhhk",       
-      "y": "Y1XKDaAyDwijp8aEIGHmO46huKjajSQH2cbfpWaWpQ4",       
-      "crv": "P-256",
-      "kid": "main-key",
-      "use": "sig",
-      "alg": "ES256"
-    }
-  ]
+  "kty": "EC",
+  "x": "3sVbr4xwN7UtmG1L19vL0x9iN-FRcl7p-Wja_xPbhhk",       
+  "y": "Y1XKDaAyDwijp8aEIGHmO46huKjajSQH2cbfpWaWpQ4",       
+  "crv": "P-256",
+  "kid": "main-key",
+  "use": "sig",
+  "alg": "ES256"
 };
 export const handler: Handler = async () => {
   return {
